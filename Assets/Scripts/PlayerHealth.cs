3fe67119f16b0b9818586ad7c4c3e9435a3b91f9
M.S.R <radityoadit9@gmail.com>
2022-12-17 15:07:21 +0700

    Player Health and starting enemy
    menambah health bar pada player dan memulai pengembangan pada musuh

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+    public HealthScript healthbar;
+
+    private void Start()
+    {
+        healthbar = GameObject.FindObjectOfType<HealthScript>();
+
+        CurrentHealth = MaxHealth;
+        healthbar.SetMaxHealth(MaxHealth);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void takeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            takeDamage(20);
+            healthbar.SetHealth(CurrentHealth);
+        }
+    }
+
+}
